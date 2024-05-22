3e58d8c3d7ac79d251aec76e768babd7e3c8644a
gabriel ferreira flora carvalheiro <gabrielfe1202@gmail.com>
2024-05-21 23:09:31 -0300

    first commmit

---
+++ ScreenSound.Web/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using ScreenSound.Web;
+using ScreenSound.Web.Servicies;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices();
+
+builder.Services.AddTransient<ArtistaAPI>();
+builder.Services.AddHttpClient("API", client => {
+    client.BaseAddress = new Uri(builder.Configuration["APIServer:Url"]!);
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
+
+
+await builder.Build().RunAsync();
